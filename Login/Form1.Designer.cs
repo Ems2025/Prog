678145e5fdf03ea9bd6d9b65a9d1b9f11130b1e3
EMILY MICHELLE DE SOUSA SANTOS <emily.mssantos1@senacsp.edu.br>
2025-02-28 22:13:02 -0300

    quinta aula

--- Login/Form1.Designer.cs
+++ Login/Form1.Designer.cs
@@ -38,19 +38,27 @@
             labelTitulo = new Label();
             pictureBox1 = new PictureBox();
             Resultado = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            button1 = new Button();
+            Cadastrar = new Label();
+            ResultadoCadastrar = new Label();
+            label6 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // TextboxLogin
             // 
-            TextboxLogin.Location = new Point(298, 81);
+            TextboxLogin.Location = new Point(429, 150);
             TextboxLogin.Name = "TextboxLogin";
             TextboxLogin.Size = new Size(100, 23);
             TextboxLogin.TabIndex = 0;
             // 
             // textboxSenha
             // 
-            textboxSenha.Location = new Point(298, 124);
+            textboxSenha.Location = new Point(429, 189);
             textboxSenha.Name = "textboxSenha";
             textboxSenha.Size = new Size(100, 23);
             textboxSenha.TabIndex = 1;
@@ -58,7 +66,7 @@
             // buttonEntrar
             // 
             buttonEntrar.BackColor = SystemColors.Control;
-            buttonEntrar.Location = new Point(308, 180);
+            buttonEntrar.Location = new Point(445, 242);
             buttonEntrar.Name = "buttonEntrar";
             buttonEntrar.Size = new Size(75, 23);
             buttonEntrar.TabIndex = 2;
@@ -69,7 +77,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(241, 85);
+            label1.Location = new Point(384, 158);
             label1.Name = "label1";
             label1.Size = new Size(37, 15);
             label1.TabIndex = 3;
@@ -78,7 +86,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(241, 132);
+            label2.Location = new Point(384, 197);
             label2.Name = "label2";
             label2.Size = new Size(39, 15);
             label2.TabIndex = 4;
@@ -107,7 +115,7 @@
             // 
             pictureBox1.BackColor = SystemColors.GradientActiveCaption;
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(-127, -270);
+            pictureBox1.Location = new Point(-86, -276);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(786, 936);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
@@ -117,11 +125,81 @@
             // Resultado
             // 
             Resultado.AutoSize = true;
-            Resultado.Location = new Point(283, 150);
+            Resultado.Location = new Point(429, 224);
             Resultado.Name = "Resultado";
             Resultado.Size = new Size(38, 15);
             Resultado.TabIndex = 8;
             Resultado.Text = "label5";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(67, 158);
+            label4.Name = "label4";
+            label4.Size = new Size(64, 15);
+            label4.TabIndex = 9;
+            label4.Text = "LabelEmail";
+            label4.Click += label4_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(142, 150);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(142, 197);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 11;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(67, 205);
+            label5.Name = "label5";
+            label5.Size = new Size(39, 15);
+            label5.TabIndex = 12;
+            label5.Text = "Senha";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(142, 260);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 13;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // Cadastrar
+            // 
+            Cadastrar.AutoSize = true;
+            Cadastrar.Location = new Point(162, 105);
+            Cadastrar.Name = "Cadastrar";
+            Cadastrar.Size = new Size(57, 15);
+            Cadastrar.TabIndex = 14;
+            Cadastrar.Text = "Cadastrar";
+            // 
+            // ResultadoCadastrar
+            // 
+            ResultadoCadastrar.AutoSize = true;
+            ResultadoCadastrar.Location = new Point(142, 224);
+            ResultadoCadastrar.Name = "ResultadoCadastrar";
+            ResultadoCadastrar.Size = new Size(38, 15);
+            ResultadoCadastrar.TabIndex = 15;
+            ResultadoCadastrar.Text = "label6";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(445, 105);
+            label6.Name = "label6";
+            label6.Size = new Size(38, 15);
+            label6.TabIndex = 16;
+            label6.Text = "Entrar";
             // 
             // Formslogin
             // 
@@ -129,6 +207,14 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ButtonFace;
             ClientSize = new Size(576, 402);
+            Controls.Add(label6);
+            Controls.Add(ResultadoCadastrar);
+            Controls.Add(Cadastrar);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
             Controls.Add(Resultado);
             Controls.Add(labelTitulo);
             Controls.Add(label3);
@@ -156,5 +242,13 @@
         private Label labelTitulo;
         private PictureBox pictureBox1;
         private Label Resultado;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label5;
+        private Button button1;
+        private Label Cadastrar;
+        private Label ResultadoCadastrar;
+        private Label label6;
     }
 }
--- Login/Form1.cs
+++ Login/Form1.cs
@@ -5,8 +5,8 @@ namespace Login
 
     public partial class Formslogin : Form
     {
-        List<string> listaUsuarios = new List<string>() { "neymar.jr", "pablo.Vittar}","sukuna.silva" }; // Criar o array de nomes de usuários
-
+        List<string> listaUsuarios = new List<string>() { "neymar.jr", "pablo.Vittar}", "sukuna.silva" }; // Criar o array de nomes de usuários
+        List<string> listaSenhas = new List<string>() { "bruna", "12345", "777" };
         public Formslogin()
         {
             InitializeComponent();
@@ -18,32 +18,32 @@ namespace Login
             string loginbuscado = TextboxLogin.Text;
             string Senha = textboxSenha.Text;
             Resultado.Text = loginbuscado;
-            
-            if(string.IsNullOrWhiteSpace(loginbuscado))
+
+            if (string.IsNullOrWhiteSpace(loginbuscado))
             {
                 Resultado.Text = "Usuario eh obrigatorio!!";
                 Resultado.ForeColor = Color.Red;
                 return;
             }
-              
-            if (Senha == null || Senha == "") 
-            
-                {
-                    Resultado.Text = " Senha eh Obrigatória !!";
-                    Resultado.ForeColor = Color.Red;
-                    return;
-                }
-                    
+
+            if (Senha == null || Senha == "")
+
+            {
+                Resultado.Text = " Senha eh Obrigatória !!";
+                Resultado.ForeColor = Color.Red;
+                return;
+            }
+
             int posicaologinEncontrado = -1;
             for (int i = 0; i < listaUsuarios.Count; i++) //3 condicoes de execucao,i representa o indice ou posicao ela é uma variavel 
-               //i acrescenta 1 toda vez que codigo roda, até chegar no 3 onde o programa para
+                                                          //i acrescenta 1 toda vez que codigo roda, até chegar no 3 onde o programa para
             {
                 if (loginbuscado == listaUsuarios[i])
                 {
                     posicaologinEncontrado = i;
                 }
             }
-                if (posicaologinEncontrado > -1 && Senha == "12345")
+            if (posicaologinEncontrado > -1 && Senha == listaSenhas[posicaologinEncontrado])
             {
                 Resultado.Text = "Autenticado com sucesso!";
                 Resultado.ForeColor = Color.Green;
@@ -55,6 +55,62 @@ namespace Login
             }
 
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {       // novas varias criadas para a parte CADASTRAR
+            string novoUsuario = TextboxLogin.Text;
+            string novaSenha = TextboxLogin.Text;
+
+            // Comando para caso o usuario deixe em branco o login
+            if(string.IsNullOrEmpty(novoUsuario))
+            {
+                ResultadoCadastrar.Text = "Usuario eh Obrigatorio!";
+                return;
+            }
+            // comando caso o usuario deixe o espaço senha em branco 
+            if (string.IsNullOrEmpty(novaSenha))
+            {
+                ResultadoCadastrar.Text = "Senha eh Obrigatoria!";
+                return;
+            }
+             // comando para verificar se há usuarios com o mesmo nome 
+            bool UsuarioEncontrado = false;
+            for (int i = 0; i < listaUsuarios.Count; i++)
+            {
+                if (novoUsuario == listaUsuarios[i])
+                {
+                    UsuarioEncontrado = true;
+
+                }
+            }
+            // comando para adicionar novo usuario com nomes diferentes a lista
+            if (UsuarioEncontrado == false)
+            {
+                listaUsuarios.Add(novoUsuario); // adiciona a lista usuario
+                listaSenhas.Add(novaSenha);// addciona a lista Senhas
+                ResultadoCadastrar.Text = "Usuario Cadastrado";
+                ResultadoCadastrar.ForeColor = Color.Green;
+          
+            }
+
+            // comando para avisar que ja esxiste usuario com o mesmo nome 
+            else
+            {
+                ResultadoCadastrar.Text = "Usuario já existente";
+                ResultadoCadastrar.ForeColor = Color.Red;
+            }
+            
+
+
+
+
+            
+        }
     }
 }
             
@@ -62,4 +118,7 @@ namespace Login
 
         
       
+
+
+
 
